af33268d1fa7261a178d78fb3e63f6ee103879b9
Hongjing Wang <tonystevenj@gmail.com>
2019-11-22 22:07:18 -0500

    cleanup

--- DataVisualization/Server/controller/report.py
+++ DataVisualization/Server/controller/report.py
@@ -36,13 +36,9 @@ def reviews():
         ret, exists = data.getItem('Reviews', id['key'])
         if exists:
             result.append([id['key'],ret])
-    re,bool = data.getObj("Reviews")# 这里为什么调用这个get函数拿到的是tuple类型？
-    # ---》原来是函数返回了2个值, 不能写re = data.getObj("Reviews")，
-    # 这样的话 re里面包含了两个对象，一个是想要的，还有一个是bool
-    # 也就是说你只写一个变量 接收返回多个值的函数的话，就会全部接收
+    re,bool = data.getObj("Reviews")
     nparray = np.array(result)
     output = re.full_process(nparray) #输入形式：(n,2)
-    # print(output.shape)  # output.shape: (n,3) n 取决于有多少个商家，目前坐标X,Y+半径0.1的话，有52个商家，(52,3)
     list_dic_out = []
     for i in range(len(output)):
         list_dic_out.append({'business_id': str(output[i,0]),
@@ -55,10 +51,6 @@ def reviews():
     whole_words={}
     for i in range(len(output)):
         shop = output[i]
-        print(shop)
-        print(type(shop))
-        print(shop[1])
-        print(shop[1][2])
         for j in range(len(shop[1])):
             if shop[1][j] in whole_words:
                 whole_words[shop[1][j]] += shop[2][j]
