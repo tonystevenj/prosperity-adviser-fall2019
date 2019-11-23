cb2479dde413ba0a97bc0da1b9fcc17c55265076
Peter <houpengg@gmail.com>
2019-11-23 17:53:25 -0500

    fix bug

--- DataVisualization/Application/config/index.js
+++ DataVisualization/Application/config/index.js
@@ -16,7 +16,7 @@ module.exports = {
         target: 'http://127.0.0.1:8080/',  // 接口域名
         changeOrigin: true,  //是否跨域
         pathRewrite: {
-          '^/api': ''   //需要rewrite重写的,
+          '^/api': '/api'   //需要rewrite重写的,
         }
       }
     },
