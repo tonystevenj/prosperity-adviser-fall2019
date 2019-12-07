ba7fa7c8fbe7d34bc172c49876bf055470c8b74f
Hongjing Wang <tonystevenj@gmail.com>
2019-12-06 19:22:45 -0500

    For Cluster, Ver 2.0

--- DataProcessing/Cluster/calculateLongestTwonode.py
+++ DataProcessing/Cluster/calculateLongestTwonode.py
@@ -4,6 +4,7 @@ import random
 import pandas as pd
 import matplotlib.pyplot  as plt
 
+
 def show_fig():
     dataSet = loadDataSet()
     fig = plt.figure()
@@ -11,34 +12,36 @@ def show_fig():
     ax.scatter(dataSet[:, 0], dataSet[:, 1])
     plt.show()
 
+
 def calcuDistance(vec1, vec2):
     # 计算向量1与向量2之间的欧式距离
-    return np.sqrt(np.sum(np.square(vec1 - vec2)))  #注意这里的减号
+    return np.sqrt(np.sum(np.square(vec1 - vec2)))  # 注意这里的减号
+
 
 def loadDataSet():
     data = pd.read_csv("Data/parks.csv").to_numpy(str).T[0:2].T
     data_convert = data.astype(float)
-    print(len(data_convert))
-    print(data_convert[0])
-    print(type(data_convert))
+    # print(len(data_convert))
+    # print(data_convert[0])
+    # print(type(data_convert))
     return data_convert
 
 
 def initCentroids(dataSet, k):
     # 从数据集中随机选取k个数据返回
     dataSet = list(dataSet)
-    print(len(dataSet))
+    # print(len(dataSet))
     return random.sample(dataSet, k)
 
-def minDistance(dataSet, centroidList):
 
+def minDistance(dataSet, centroidList):
     # 对每个属于dataSet的item， 计算item与centroidList中k个质心的距离，找出距离最小的，并将item加入相应的簇类中
-    clusterDict = dict() #dict保存簇类结果
+    clusterDict = dict()  # dict保存簇类结果
     k = len(centroidList)
     for item in dataSet:
         vec1 = item
         flag = -1
-        minDis = float("inf") # 初始化为最大值
+        minDis = float("inf")  # 初始化为最大值
         for i in range(k):
             vec2 = centroidList[i]
             distance = calcuDistance(vec1, vec2)  # error
@@ -47,16 +50,17 @@ def minDistance(dataSet, centroidList):
                 flag = i  # 循环结束时， flag保存与当前item最近的蔟标记
         if flag not in clusterDict.keys():
             clusterDict.setdefault(flag, [])
-        clusterDict[flag].append(item)  #加入相应的类别中
-    return clusterDict  #不同的类别
+        clusterDict[flag].append(item)  # 加入相应的类别中
+    return clusterDict  # 不同的类别
+
 
 def getCentroids(clusterDict):
-    #重新计算k个质心
+    # 重新计算k个质心
     centroidList = []
     for key in clusterDict.keys():
         centroid = np.mean(clusterDict[key], axis=0)
         centroidList.append(centroid)
-    return centroidList  #得到新的质心
+    return centroidList  # 得到新的质心
 
 
 def getVar(centroidList, clusterDict):
@@ -72,13 +76,14 @@ def getVar(centroidList, clusterDict):
         sum += distance
     return sum
 
+
 def showCluster(centroidList, clusterDict):
     # 展示聚类结果
-    colorMark = ['or', 'ob', 'og', 'ok', 'oy', 'ow','oc'] #不同簇类标记，o表示圆形，另一个表示颜色
-    centroidMark = ['dr', 'db', 'dg', 'dk', 'dy', 'dw','dc']
+    colorMark = ['or', 'ob', 'og', 'ok', 'oy', 'ow', 'oc']  # 不同簇类标记，o表示圆形，另一个表示颜色
+    centroidMark = ['dr', 'db', 'dg', 'dk', 'dy', 'dw', 'dc']
 
     for key in clusterDict.keys():
-        plt.plot(centroidList[key][0], centroidList[key][1], centroidMark[key], markersize=12) #质心点
+        plt.plot(centroidList[key][0], centroidList[key][1], centroidMark[key], markersize=12)  # 质心点
         for item in clusterDict[key]:
             plt.plot(item[0], item[1], colorMark[key])
 
@@ -99,29 +104,36 @@ def test_k_means():
         oldVar = newVar
         newVar = getVar(centroidList, clusterDict)
         times += 1
-        showCluster(centroidList, clusterDict)
-    print("各类长度:")
+        # showCluster(centroidList, clusterDict)
+    showCluster(centroidList, clusterDict)
+    # print("各类长度:")
+    # for key in clusterDict.keys():
+    #     print(len(clusterDict[key]))
+    #     print(clusterDict[key])
+
+    # 改变字典储存方式:
+    clusterList=[]
     for key in clusterDict.keys():
-        print(len(clusterDict[key]))
-if __name__ == '__main__':
-    # show_fig()
-    test_k_means()
+        clusterList.append(clusterDict[key])
+    return clusterList
 
 
-# *************************************分隔符***********************************************************8
+
+
 def JarvisMarch(S):
     S = np.array(S)
+
     def angle(u, v):
         x = v[0] - u[0]
         y = v[1] - u[1]
-        if y<0:
+        if y < 0:
             class23 = True
         else:
             class23 = False
-        if x<0 and y>0:
+        if x < 0 and y > 0:
             class4 = True
         else:
-            class4=False
+            class4 = False
         if y == 0:
             if x == 0:
                 return 361
@@ -131,14 +143,15 @@ def JarvisMarch(S):
                 return 270
         out = math.atan(x / y) / math.pi * 180
         if class23:
-            out+=180
+            out += 180
         if class4:
-            out+=360
+            out += 360
         return out
+
     leftmost = 0
     for i in range(len(S)):
         node = S[i]
-        if S[leftmost][0]> node[0]:
+        if S[leftmost][0] > node[0]:
             leftmost = i
     convexHull = []
 
@@ -150,16 +163,16 @@ def JarvisMarch(S):
         minnextDegree = None
         first = True
         for i in range(len(S)):
-            nextDegree = angle(S[nownode],S[i])
-            plusDegree = nextDegree-nowDegree
+            nextDegree = angle(S[nownode], S[i])
+            plusDegree = nextDegree - nowDegree
 
-            if plusDegree<0:
+            if plusDegree < 0:
                 continue
             if first == True:
                 minplusDegree = plusDegree
                 first = False
-            if plusDegree<=minplusDegree:
-                minplusDegree=plusDegree
+            if plusDegree <= minplusDegree:
+                minplusDegree = plusDegree
                 minnextDegree = nextDegree
                 nextnode = i
         if nextnode == leftmost:
@@ -168,23 +181,38 @@ def JarvisMarch(S):
         nowDegree = minnextDegree
     return convexHull
 
-def destance(u,v):
+def destance(u, v):
     x = v[0] - u[0]
     y = v[1] - u[1]
-    return math.sqrt(pow(x,2)+pow(y,2))
+    return math.sqrt(pow(x, 2) + pow(y, 2))
 
-data = pd.read_csv("Data/parks.csv").to_numpy(str).T[0:2].T
-data_convert=data.astype(float).tolist()
-convexID = JarvisMarch(data_convert)
-print("输出：",convexID)
 
-# 计算最长两点间最长距离：
-longest = 0
-for i in range(len(convexID)):
-    convex1 = data_convert[convexID[i]]
-    for j in range(len(convexID)):
-        convex2=data_convert[convexID[j]]
-        longest = max(longest,destance(convex1,convex2))
-print(longest)
+def calculateDensity(data_2dPoints):
+    convexID = JarvisMarch(data_2dPoints)
+    print("环边输出：", convexID)
+    # 计算最长两点间最长距离：
+    longest = 0
+    for i in range(len(convexID)):
+        convex1 = data_2dPoints[convexID[i]]
+        for j in range(len(convexID)):
+            convex2 = data_2dPoints[convexID[j]]
+            longest = max(longest, destance(convex1, convex2))
+    print("直径:",longest)
 
-# 计算密度：
+    # 计算密度：
+    amount = len(data_2dPoints)
+    print("总数量",amount)
+    area = math.pi*pow(longest,2)
+    density = amount/area
+    return density
+
+
+# 全部数据：
+# data = pd.read_csv("Data/parks.csv").to_numpy(str).T[0:2].T
+# data_convert = data.astype(float).tolist()
+# print(calculateDensity(data_convert))
+
+# cluster分类：
+clusterList=test_k_means()
+for data in clusterList:
+    print("密度：",calculateDensity(data))
