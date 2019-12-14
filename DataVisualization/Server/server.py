f6777d6ac098250769593a51375b0d60b0349a6f
Hongjing Wang <tonystevenj@gmail.com>
2019-12-14 15:18:34 -0500

    For hugo: 在hugo生成的index.html中,我手动吧所有的文件应用前面都加了个static/ 然后刷新,没有渲染 server.py里面去掉static_url_path='' ,刷新,有渲染

--- DataVisualization/Server/server.py
+++ DataVisualization/Server/server.py
@@ -4,6 +4,6 @@ from . import router
 
 
 def run():
-    app = Flask(__name__,static_url_path='')
+    app = Flask(__name__)
     router.dispatch(app)
     app.run(host='0.0.0.0', port=8080, debug=True)
